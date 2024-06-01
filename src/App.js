6e54016fe12ffb798d411c5488bb12bcf7303811
mdealiaga <matthiasdealiaga@gmail.com>
2024-06-02 00:40:23 +0100

    added download link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import DataTable from './components/DataTable';
-import './App.css'; 
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Game Netcode Database</h1>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px' }}>
+        <h1 style={{ margin: 0 }}>Game Netcode Database</h1>
+        <a
+          href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSev34FK3e5MuHtQED5AUSovGEAU9l5TgxP4_w-RnEQRngIM6EDBRvzPS7WJnWKHjPrzMsl9BlCI1ly/pub?gid=133993680&single=true&output=csv"
+          download
+          style={{ marginLeft: '10px', display: 'flex', alignItems: 'center' }}
+        >
+          <img src={`${process.env.PUBLIC_URL}/download-icon.png`} alt="Download CSV" style={{ width: '24px', height: '24px' }} />
+        </a>
+      </div>
       <DataTable />
     </div>
   );
